87445433f755bdd45d7b926ab5a97b9dcaa5c367
Frank Kilkelly <frank.kilkelly@gmail.com>
2017-11-02 19:37:06 +0000

    Add option to display without LSK amount decimal places

--- src/index.js
+++ src/index.js
@@ -3,21 +3,43 @@
 import BigNumber from 'bignumber.js'
 import numeral from 'numeral'
 
+// ---------------------------------------------------
+
 function formatLskValueWithDecimalPoint (lskValue) {
   return new BigNumber(lskValue || 0).dividedBy(new BigNumber(10).pow(8)).toFixed()
 }
+
+// ---------------------------------------------------
 
 const formatLskValueWithCommas = function (lskValue) {
   return numeral(lskValue).format('0,0.[0000000000000]')
 }
 
-function liskPrettify_ (rawLskValue, { commas = true } = {}) {
+// ---------------------------------------------------
+
+function liskPrettify_ (
+  rawLskValue,
+  {
+    commas = true, // include commas? e.g. '4,002.51338358'
+    decimals = true // include decimal places? e.g. 4002.51338358
+  } = {}
+) {
+  let rawLskValueInProcess = formatLskValueWithDecimalPoint(rawLskValue)
+
+  // Don't show decimal places
+  if (!decimals) {
+    rawLskValueInProcess = Math.floor(rawLskValueInProcess)
+  }
+
+  // Include commas in the value (outputs a string)
   if (commas) {
-    return formatLskValueWithCommas(formatLskValueWithDecimalPoint(rawLskValue))
-  } else {
-    return parseFloat(formatLskValueWithDecimalPoint(rawLskValue))
+    return formatLskValueWithCommas(rawLskValueInProcess)
+  } else { // No commas in the value
+    return parseFloat(rawLskValueInProcess)
   }
 }
+
+// ---------------------------------------------------
 
 export default liskPrettify_
 export const liskPrettify = liskPrettify_
--- test.js
+++ test.js
@@ -20,3 +20,23 @@ test('Prettify small amount, no commas', function (t) {
   t.plan(1)
   t.equal(liskPrettify(123, { commas: false }), 0.00000123)
 })
+
+test('Prettify amount, no decimal places', function (t) {
+  t.plan(1)
+  t.equal(liskPrettify(400251338358, { decimals: false }), '4,002')
+})
+
+test('Prettify small amount, no decimal places', function (t) {
+  t.plan(1)
+  t.equal(liskPrettify(123, { decimals: false }), '0')
+})
+
+test('Prettify amount, no commas, no decimal places', function (t) {
+  t.plan(1)
+  t.equal(liskPrettify(400251338358, { commas: false, decimals: false }), 4002)
+})
+
+test('Prettify small amount, no commas, no decimal places', function (t) {
+  t.plan(1)
+  t.equal(liskPrettify(123, { commas: false, decimals: false }), 0)
+})
